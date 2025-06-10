8c9e2647f59c909a353b31abc926fd684dbd9a77
dan-hargreaves <71017997+dan-hargreaves@users.noreply.github.com>
2025-06-10 17:09:40 +0200

    Delete Chess Game/Chess Game directory

--- Chess Game/Chess Game/ChessGame/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from .GUI import GUI
-from .start import start
-from .Game import Game
-
-# __all__ = ['add', 'subtract']
--- Chess Game/Chess Game/ChessGame/start.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import ChessGame
-# sys.path.insert(1,'C:/Users/dhargreaves/OneDrive - Foresight Group LLP/Documents/Misc/Python files/Chess Game/Chess Game/Classes/')
-
-def start():
-    GUI = ChessGame.GUI()
-    GUI.mainMenu()
