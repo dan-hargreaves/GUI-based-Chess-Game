cac2e03607f43b8f4152206ed5fc05e913ac08d8
dan hargreaves <d.hargreaves@icloud.com>
2025-11-18 09:48:41 +0000

    Test git commit via GIT Bash

--- Chess Game/start_game.py
+++ Chess Game/start_game.py
@@ -4,6 +4,7 @@ import ChessGame
 # sys.path.insert(1,'C:/Users/dhargreaves/OneDrive - Foresight Group LLP/Documents/Misc/Python files/Chess Game/Chess Game/Classes/')
 
 # %%
+print('GIT_test_change')
 GUI = ChessGame.start()
 # %%
 # %%
